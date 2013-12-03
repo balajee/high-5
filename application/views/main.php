6a7befee4ef9971f05b67fdacfc15b7e7b528adb
Balajee Dhanapal <globalbala@yahoo.com>
2013-12-03 08:00:49 +0530

    created time based video in home page

--- application/views/main.php
+++ application/views/main.php
@@ -1,26 +1,16 @@
-<div id="recommend_mn" class="row">
-	<div class="col-lg-12">
-		<section class="panel">
-		    <div class="panel-body">
-			<div id="recommend"><i></i></div>
-		    </div>
-		</section>
-	</div>
-</div>
-
 <div class="row">
 	<div class="col-lg-12">
 		<? if ($records) { ?>
 			<section class="panel">
-				<heading class="panel-heading">
+				<header class="panel-heading">
 					<?= $welcome_msg ?>
-				</heading>
+				</header>
 				<div class="panel-body">
 					<?
 					$i = 0;
 					foreach ($records['items'] as $record) { ?>
 						<? if ($i==0) {  ?>
-							<video width="100%" height="300" controls>
+							<video width="100%" controls>
 								<source src="<?= $record['videoUrl'] ?>" type="video/mp4">
 								<source src="movie.ogg" type="video/ogg">
 								Your browser does not support the video tag.
@@ -33,5 +23,15 @@
 				</div>
 			</section>
 		<? } ?>
+	</div>
+</div>
+
+<div id="recommend_mn" class="row">
+	<div class="col-lg-12">
+		<section class="panel">
+		    <div class="panel-body">
+			<div id="recommend"><i></i></div>
+		    </div>
+		</section>
 	</div>
 </div>
