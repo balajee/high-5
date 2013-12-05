c835fca1a5851a3f9cc25497e0cccd55173238e4
sitaram81581 <sitaram81581@gmail.com>
2013-12-05 19:36:33 +0530

    Added new partners data

--- application/helpers/utilis_helper.php
+++ application/helpers/utilis_helper.php
@@ -59,15 +59,20 @@ function initPartners() {
 	$newsArray = array();
 	$newsArray[0] = new Partner("cnet", "CNET", "42.363766", "-71.079521", "http://on.aol.com/partner/cnet-251736463", "News");
 	$newsArray[1] = new Partner("associatedpress", "AP", "40.75332", "-73.99947", "http://on.aol.com/partner/associated-press-516990341", "News");
+	$newsArray[2] = new Partner("reuters", "REUTERS", "40.756457", "-73.98698", "http://on.aol.com/partner/reuters-517199251", "News");
+	$newsArray[3] = new Partner("time", "TIME", "40.760734", "-73.981009", "http://on.aol.com/partner/time-517930617", "News");
 
 	$entArray = array();
 	$entArray[0] = new Partner("e!", "E! Online", "33.563962", "-112.059585", "http://on.aol.com/partner/e--280071933", "Entertainment");
 	$entArray[1] = new Partner("splashnews", "Splash News", "40.728381", "-73.993696", "http://on.aol.com/partner/splashnews-246666329", "Entertainment");
+	$entArray[2] = new Partner("people", "PEOPLE", "40.758739", "-73.97867", "http://on.aol.com/partner/people-517961076", "Entertainment");
+	$entArray[3] = new Partner("popsugarrush", "POPSUGAR Celebrity", "40.743063", "-73.984585", "http://on.aol.com/partner/popsugarrush-277116489", "Entertainment");
 
 	$fashArray = array();
 	$fashArray[0] = new Partner("elle", "ELLE", "40.766875", "-73.983675", "http://on.aol.com/partner/elle-517835012", "Fashion");
 	$fashArray[1] = new Partner("birchbox", "BIRCHBOX", "40.744103", "-73.984855", "http://on.aol.com/partner/birchbox-517685889", "Fashion");
 	$fashArray[2] = new Partner("lucky", "Lucky Magazine", "40.756086", "-73.985945", "http://on.aol.com/partner/lucky-69785807", "Fashion");
+	$fashArray[3] = new Partner("vogue", "VOGUE", "40.756114", "-73.985973", "http://on.aol.com/partner/vogue-517961212", "Fashion");
 
 	$busArray = array();
 	$busArray[0] = new Partner("wsjlive", "The Wall Street Journal", "29.984188", "-95.335437", "http://on.aol.com/partner/wsjlive-517302109", "Business");
